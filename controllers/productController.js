0b2a522b670cba42d754ec7cf52601a297ca892b
eersya <eersyaando@gmail.com>
2021-12-13 21:26:55 -0600

    funcionalidad agregada

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 
+const DB_FILENAME = 'productos.json';
 const productos = require('./productos');
 
 const path = require('path');
@@ -6,7 +7,15 @@ const fs = require('fs');
 
 const multer = require('multer');
 
+function saveProducts(ps) {
+    fs.writeFileSync(DB_FILANAME, JSON.stringify(ps));
+}
+
 const productControllers = {
+
+    viewProducts: function(req, res) {
+        res.render('home', { productos: productos });
+    },
 
     detailProduct: function(req, res) {
         const id = req.params.id;
@@ -21,15 +30,37 @@ const productControllers = {
     },
 
     createProduct: function(req, res) {
-        
+        const body = req.body;
+
+        const newProduct = {
+            name : body.name,
+            avaibleColors : body.colors,
+            price : body.price,
+            categories : body.categories,
+            withDiscount : body.withDiscount,
+            discount : body.discount
+        };
+
+        const newProducts = products.push(newProduct);
+        saveProducts(newProducts);
+
     },
 
     deleteProduct: function(req, res) {
         const id = req.params.id;
 
+        const newProducts = products.filter(p => p.id == id);
+        saveProducts(newProducts);
+
+        viewProducts(req, res);
+
     },
 
     updateProduct: function(req, res) {
+        const id = req.params.id;
+
+        const filteredProducts = products.filter(p => p.id == id);
+
         
     }
     
