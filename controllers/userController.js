c86ac773534195a8729dfd6ce06150004201c339
franciscominajas <franciscominajas@gmail.com>
2021-12-13 21:05:33 -0600

    user controller

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const users = require('./users');
+
+const path = require('path');
+const fs = require('fs');
+
+const userController = {
+    detailUser = function(req,res){
+        const  id = req.params.id;
+        const product = productos.find( item => item.id == id);
+
+        if(user === null){
+            res.send("No existe este usuario");
+        }else{
+            res.send("detallesDeUsuario",{user: users});
+        }
+    }
+
+    createUser: function(req,res){
+
+    },
+
+    deleteUser: function(req,res){
+        const id = req.params.id;
+    },
+
+    updateUser: function(req,res){
+
+    }
+};
+
+module.exports = userController;
