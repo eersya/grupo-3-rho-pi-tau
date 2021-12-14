80bf8de170f3e0901dba4c848227c7c2a8355aa5
franciscominajas <franciscominajas@gmail.com>
2021-12-13 20:34:26 -0600

    archivo de usuarios

---
+++ controllers/usuarios.js
@@ -0,0 +1,95 @@
+usuarios = [
+    {
+        id: 1,
+        nombre: 'Francisco',
+        apellidoP: 'Minajas',
+        apellidoM: 'Carbajal',
+        edad: 29,
+        email: 'javier@gmail.com',
+        activo: true,
+        fechaNacimiento: 20/11/2000,
+        tipoUsuario: "usuario"
+    },{
+        id: 2,
+        nombre: 'Erika',
+        apellidoP: 'Jose',
+        apellidoM: 'Juarez',
+        edad: 23,
+        email: 'ericka@gmail.com',
+        activo: false,
+        fechaNacimiento: 20/08/1992,
+        tipoUsuario: "admin"
+    },{
+        id: 3,
+        nombre: 'Juanito',
+        apellidoP: 'Lozada',
+        apellidoM: 'Pineda',
+        edad: 20,
+        email: 'ericka@gmail.com',
+        activo: true,
+        fechaNacimiento: 20/01/1989,
+        tipoUsuario: "admin"
+    },{
+        id: 4,
+        nombre: 'Juanito',
+        apellidoP: 'Lozada',
+        apellidoM: 'Pineda',
+        edad: 28,
+        email: 'ericka@gmail.com',
+        activo: false,
+        fechaNacimiento: 20/02/2012,
+        tipoUsuario: "usuario"
+    },{
+        id: 5,
+        nombre: 'Pepe',
+        apellidoP: 'Perez',
+        apellidoM: 'Perez',
+        edad: 19,
+        email: 'pepe@gmail.com',
+        activo: true,
+        fechaNacimiento: 11/03/2002,
+        tipoUsuario: "vendedor"
+    },{
+        id: 6,
+        nombre: 'Aurora',
+        apellidoP: 'Juarez',
+        apellidoM: 'Lopez',
+        edad: 18,
+        email: 'aurora@gmail.com',
+        activo: false,
+        fechaNacimiento: 11/03/1996,
+        tipoUsuario: "vendedor"
+    },{
+        id: 7,
+        nombre: 'Arely',
+        apellidoP: 'Lopez',
+        apellidoM: 'Juarez',
+        edad: 17,
+        email: 'arely@gmail.com',
+        activo: true,
+        fechaNacimiento: 11/03/1966,
+        tipoUsuario: "usuario"
+    },{
+        id: 8,
+        nombre: 'Danae',
+        apellidoP: 'Juarez',
+        apellidoM: 'Lopez',
+        edad: 15,
+        email: 'danae@gmail.com',
+        activo: false,
+        fechaNacimiento: 11/03/1952,
+        tipoUsuario: "usuario"
+    },{
+        id: 9,
+        nombre: 'Jose',
+        apellidoP: 'Lopez',
+        apellidoM: 'Lopez',
+        edad: 16,
+        email: 'jose.lopez@gmail.com',
+        activo: true,
+        fechaNacimiento: 11/03/1942,
+        tipoUsuario: "admin"
+    }
+]
+
+module.exports = usuarios;
