7009c546272b5c6dfa27f98dfc13f743c4656b3e
eersya <eersyaando@gmail.com>
2021-12-13 20:38:20 -0600

    controladores de producto acutualizado

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,38 @@
 
+const productos = require('./productos');
+
+const path = require('path');
 const fs = require('fs');
+
 const multer = require('multer');
 
-const productControllers = [
+const productControllers = {
 
+    detailProduct: function(req, res) {
+        const id = req.params.id;
+        const product = productos.find( item => item.id == id);
+
+        if (product === null) {
+            res.send("No existe este producto");
+        } else {
+            res.sender('detallesDelProducto', { producto: producto });
+        }
+
+    },
+
+    createProduct: function(req, res) {
+        
+    },
+
+    deleteProduct: function(req, res) {
+        const id = req.params.id;
+
+    },
+
+    updateProduct: function(req, res) {
+        
+    }
     
-    
-];
+};
 
 module.exports = productControllers;
