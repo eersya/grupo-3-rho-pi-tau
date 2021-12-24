4a4587c41fd26557616b54a8ae357d5da3ea07f9
franciscominajas <franciscominajas@gmail.com>
2021-12-23 20:04:20 -0600

    carrito router

--- routers/carritoRouter.js
+++ routers/carritoRouter.js
@@ -2,22 +2,14 @@ const express = require('express');
 const carritoRouter = express.Router();
 const carritoController = require('../controllers/carritoController');
 
-// Detalles del producto
 carritoRouter.get('/detail/:id', carritoController.detailProduct);
 
-
-// Creación del proucto
 carritoRouter.get('/create', carritoController.createProductView);
-
 carritoRouter.post('/create', carritoController.createProduct);
 
-// Modificar producto
 carritoRouter.get('/update', carritoController.updateProductView);
-
 carritoRouter.put('/update', carritoController.updateProduct);
 
-
-// Borrar producto
 carritoRouter.delete('/delete/:id', carritoController.deleteProduct);
 
 
