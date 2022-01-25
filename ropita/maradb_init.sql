3d62068fcf0404091bd0e3e844656204eecd562d
eersya <eersyaando@gmail.com>
2022-01-24 19:51:19 -0600

    aueoee

---
+++ ropita/maradb_init.sql
@@ -0,0 +1,15 @@
+
+
+create database ropita;
+
+create table ropita
+  (
+    id int auto_increment primary key,
+    name varchar(20),
+    price decimal(12, 2),
+    in_discount boolean default false,
+    discount tinyint
+  );
+
+CREATE USER 'ropita_user'@'localhost' IDENTIFIED BY '89911992';
+GRANT ALL PRIVILEGES ON todo.* TO 'ropita_user'@'localhost';
