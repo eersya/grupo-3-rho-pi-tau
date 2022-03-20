abf9ae688516e6c8b254c32a9ab54e4aa1bac66b
eersya <eersyaando@gmail.com>
2022-03-19 23:24:31 -0600

    building frome scratch

---
+++ database_configuration.js
@@ -0,0 +1,8 @@
+const DATABASE_CONFIGURATIONS = {
+    "development": {
+        "dialect": "sqlite3",
+        "database": "db.sqlite"
+    }
+};
+
+exports.DATABASE_CONFIGURATIONS = DATABASE_CONFIGURATIONS;
